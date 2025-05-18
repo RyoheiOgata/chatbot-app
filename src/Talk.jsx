d39f232edd6251e7fc17e2116c97570bfdda5066
ryohei.o <c2h8o3n@gmail.com>
2025-05-18 11:02:12 +0900

    テキスト送信後のUI改善

--- src/Talk.jsx
+++ src/Talk.jsx
@@ -1,24 +1,15 @@
 import Box from '@mui/material/Box';
 import { Bubble } from "./Bubble";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            suspense: true,
-        }
-    }
-});
 
 export function Talk({ history }) {
 
     return (
         <Box >
             {history.map((h, i) => (
-                <QueryClientProvider client={queryClient} key={i}>
-                    <Bubble
-                        line={h} />
-                </QueryClientProvider>
+                <Bubble
+                    line={h}
+                    key={i} />
             ))}
         </Box>
     )
